628c9ff914203ec7a2d99c0363fb08db0f8ee031
ttmy03 <ttmy03@github.com>
2026-05-29 22:10:35 +0200

    Externalize Cloud Tasks SDK

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["localhost", "127.0.0.1"],
+  serverExternalPackages: ["@google-cloud/tasks"],
   images: {
     remotePatterns: [
       {
